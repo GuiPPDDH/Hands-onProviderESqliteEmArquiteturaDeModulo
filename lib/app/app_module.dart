95eefd7053dc34d4c41d20e47bee1c3921eee98f
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-09-01 16:04:49 -0300

    Configurando SqliteAdmConnection

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/app_widget.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_migration_factory.dart';
 
-class AppModule extends StatelessWidget {
+class AppModule extends StatefulWidget {
   const AppModule({super.key});
+
+  @override
+  State<AppModule> createState() => _AppModuleState();
+}
+
+class _AppModuleState extends State<AppModule> {
+  final sqliteAdmConnection = SqliteAdmConnection();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(sqliteAdmConnection);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(sqliteAdmConnection);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         Provider(
-          create: (_) => SqliteMigrationFactory(),        
+          create: (_) => SqliteMigrationFactory(),
         ),
-      ], 
+      ],
       child: AppWidget(),
     );
   }
