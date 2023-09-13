6f354f94bb8335294793f79998eda2b8e47d318c
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-09-13 16:58:07 -0300

    Customizando o TextFormField

---
+++ lib/app/core/widget/todo_list_field.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import '../ui/todo_list_icons.dart';
+
+class TodoListField extends StatelessWidget {
+  final String label;
+  final bool obscureText;
+  final IconButton? suffixIconButton;
+  final ValueNotifier<bool> obscureTextVN;
+  final TextEditingController? controller;
+  final FormFieldValidator? validator;
+
+  TodoListField({
+    super.key,
+    required this.label,
+    this.obscureText = false,
+    this.suffixIconButton,
+    this.controller,
+    this.validator,
+  })  : assert(obscureText == true ? suffixIconButton == null : true,
+            'ObscureText não pode ser enviado em conjunto com suffixIconButton'),
+        obscureTextVN = ValueNotifier(obscureText);
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<bool>(
+      valueListenable: obscureTextVN,
+      builder: (_, obscureTextValue, __) {
+        return TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            labelText: label,
+            labelStyle: TextStyle(
+              fontSize: 15,
+              color: Colors.black,
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: BorderSide(color: Colors.red),
+            ),
+            isDense: true,
+            suffixIcon: suffixIconButton ??
+                (obscureText == true
+                    ? IconButton(
+                        onPressed: () {
+                          obscureTextVN.value = !obscureTextValue;
+                        },
+                        icon: Icon(
+                          !obscureTextValue ? TodoListIcons.eye_slash : TodoListIcons.eye,
+                          size: 15,
+                        ),
+                      )
+                    : null),
+          ),
+          obscureText: obscureTextValue,
+          validator: validator,
+        );
+      },
+    );
+  }
+}
