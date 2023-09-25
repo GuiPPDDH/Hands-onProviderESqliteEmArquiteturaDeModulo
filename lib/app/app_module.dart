aede5a66ca9d7f76925cb831f87b5ea4a231ef15
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-09-25 15:54:15 -0300

    Construindo Repository e Service de usuário

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/app_widget.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_adm_connection.dart';
 import 'package:todo_list_provider/app/core/database/sqlite_migration_factory.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
+import 'package:todo_list_provider/app/repositories/user/user_repository_impl.dart';
+import 'package:todo_list_provider/app/service/user/user_service.dart';
+import 'package:todo_list_provider/app/service/user/user_service_impl.dart';
 
 class AppModule extends StatefulWidget {
   const AppModule({super.key});
@@ -33,8 +38,17 @@ class _AppModuleState extends State<AppModule> {
         Provider(
           create: (_) => SqliteMigrationFactory(),
         ),
+        Provider(
+          create: (_) => FirebaseAuth.instance,
+        ),
+        Provider<UserRepository>(
+          create: (context) => UserRepositoryImpl(firebaseAuth: context.read()),
+        ),
+        Provider<UserService>(
+          create: (context) => UserServiceImpl(userRepository: context.read()),
+        ),
       ],
-      child: AppWidget(),
+      child: const AppWidget(),
     );
   }
 }
