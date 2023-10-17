6ce2f375989419f7ed45c0f3bc68304bf2101a1f
Guilherme Potter Petry <guipotterrp@gmail.com>
2023-10-17 19:52:57 -0300

    Criando navegação sem Contexto e configurando home e splash

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_provider/app/core/navigator/todo_list_navigator.dart';
 import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
 import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
+
+import 'modules/home/home_module.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -10,10 +13,11 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Todo List Provider',
-      initialRoute: '/login',
       theme: TodoListUiConfig.theme,
+      navigatorKey: TodoListNavigator.navigatorKey,
       routes: {
         ...AuthModule().routers,
+        ...HomeModule().routers,
       },
       home: const SplashPage(),
     );
